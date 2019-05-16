3dff2b597ab5d16666c7d4af0ba2634f6dfb0138
Jared Hanson <jaredhanson@gmail.com>
2019-05-16 09:16:47 -0700

    Update dev dependencies.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,11 @@
 /* global describe, it, expect */
 
-var pkg = require('..');
-
 describe('bixby-redis', function() {
   
-  it('should export object', function() {
-    expect(pkg).to.be.an('function');
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
   });
   
 });
