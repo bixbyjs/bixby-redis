e8a715490c444d780e2242e8f81df1df35e14aa5
Jared Hanson <jaredhanson@gmail.com>
2019-05-18 18:22:00 -0700

    Add tests.

--- test/service.test.js
+++ test/service.test.js
@@ -31,7 +31,8 @@ describe('service', function() {
     
     describe('.createConnection', function() {
       beforeEach(function() {
-        sinon.stub(_keyring, 'get').yieldsAsync(null, { password: 'keyboard cat' });
+        sinon.stub(_keyring, 'get').withArgs('redis.example.com').yieldsAsync(null, { password: 'keyboard cat' })
+                                   .withArgs('localhost').yieldsAsync(null, { password: 'h0m35w337h0m3' });
         
         _client.auth.callsFake(function() {
           var self = this;
@@ -69,6 +70,18 @@ describe('service', function() {
         expect(createClientStub).to.have.been.calledOnceWithExactly(6379, 'redis.example.com');
         expect(client).to.be.an.instanceof(redis.RedisClient);
       }); // should construct client, add listener and auth
+      
+      it('should construct client and auth to address', function(done) {
+        var client = api.createConnection({ name: 'localhost', address: '127.0.0.1', port: 6379 });
+        
+        expect(createClientStub).to.have.been.calledOnceWithExactly(6379, '127.0.0.1');
+        expect(client).to.be.an.instanceof(redis.RedisClient);
+        
+        client.once('connect', function() {
+          expect(client.auth).to.have.been.calledOnceWithExactly('h0m35w337h0m3');
+          done();
+        });
+      }); // should construct client and auth to address
       
     }); // .createConnection
     
