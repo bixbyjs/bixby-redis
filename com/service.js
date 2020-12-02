30cb7d16a0eccd1039091058c7c2616fa8d022d4
Jared Hanson <jaredhanson@gmail.com>
2020-12-02 10:53:55 -0800

    Add comments.

--- com/service.js
+++ com/service.js
@@ -1,10 +1,25 @@
+/**
+ * Redis.
+ *
+ * This component provides an API for querying [Redis][1], a data structure
+ * store used as a database, cache, and message broker.
+ *
+ * Internally, the implementation uses the [`redis`][2] module.  The intent of
+ * this API is to provide a wrapper that conforms more closely to Node.js'
+ * [`'net`][3] module.
+ *
+ * [1]: https://redis.io/
+ * [2]: https://github.com/NodeRedis/node-redis
+ * [3]: https://nodejs.org/api/net.html
+ */
 exports = module.exports = function(keyring) {
   var redis = require('redis');
   
   
-  var api = {};
+  var API = {};
   
-  api.createConnection = function(options, connectListener) {
+  API.connect =
+  API.createConnection = function(options, connectListener) {
     var client = redis.createClient(options.port, options.address || options.name);
     if (connectListener) { client.once('connect', connectListener); }
     
@@ -21,7 +36,7 @@ exports = module.exports = function(keyring) {
     return client;
   };
   
-  return api;
+  return API;
 };
 
 exports['@singleton'] = true;
